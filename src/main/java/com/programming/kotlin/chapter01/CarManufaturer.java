3d72e069a3772e2c1e269a5279825f09f19efebd
goodjwon <jwon76@naver.com>
2019-03-09 11:18:11 +0900

    first commit

---
+++ src/main/java/com/programming/kotlin/chapter01/CarManufaturer.java
@@ -0,0 +1,13 @@
+package com.programming.kotlin.chapter01;
+
+public class CarManufaturer {
+    private final String name;
+
+    public CarManufaturer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/kotlin/com/programming/kotlin/chapter01/Program.kt
@@ -0,0 +1,14 @@
+package com.programming.kotlin.chapter01
+
+fun main(args: Array<String>) {
+    println("Hello World");
+
+    var student = Student("JwonWon");
+
+    println("Student name : ${student.name}");
+
+    var carManufaturer = CarManufaturer("Mercedes");
+
+    println("Car manufaturer: ${carManufaturer.name}");
+
+}
