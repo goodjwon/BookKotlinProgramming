ff2202feb96e6f639f9d51c2db4e62c4eb7b893d
goodjwon <firstman9@protonmail.com>
2022-01-30 18:18:25 +0900

    use when keyword

--- src/main/kotlin/com/programming/kotlin/chapter00/Main.kt
+++ src/main/kotlin/com/programming/kotlin/chapter00/Main.kt
@@ -1,11 +1,18 @@
 package com.programming.kotlin.chapter00
 
+var name: String = "Nate"
+var greeting: String? = "hello"
+var now: String? = null
 
 fun main(){
+    now = "Sunday"
+    when(now){
+        null -> println("Hi")
+        else -> println(now)
+    }
     println("hello Kotlin")
-
-    var name : String = "Nate"
-    name = "John"
-
     println(name)
+    println(greeting)
+    greeting = null
+    println(greeting)
 }
