63629d9bcf714dd32e458eb6dd7455fe83830a67
goodjwon <firstman9@protonmail.com>
2022-02-07 22:28:18 +0900

    use when keyword

--- src/main/kotlin/com/programming/kotlin/chapter00/Main.kt
+++ src/main/kotlin/com/programming/kotlin/chapter00/Main.kt
@@ -6,6 +6,10 @@ var now: String? = null
 
 fun main(){
     now = "Sunday"
+
+    val nowToprint = if(now != null) now else "Hi"
+    println(nowToprint)
+
     when(now){
         null -> println("Hi")
         else -> println(now)
